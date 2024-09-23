b0d9e8008788be6cf177e6649ef31acb5ba184ec
leorvergani <vergani@outlook.com.br>
2024-09-23 03:09:15 -0300

    update

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
 
-export default function App() {
+import {
+  createMaterialTopTabNavigator,
+  createAppContainer,
+  MaterialTopTabBar,
+} from 'react-navigation';
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+function SafeAreaMaterialTopTabBar(props) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <MaterialTopTabBar {...props} />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const mainNavigation = createMaterialTopTabNavigator(
+  {
+    Login,
+    Dashboard,
   },
-});
+  {
+    tabBarComponent: SafeAreaMaterialTopTabBar,
+    tabBarOptions: {
+      tabStyle: {
+        backgroundColor: '#7159c1',
+      },
+    },
+  },
+);
+
+export default createAppContainer(mainNavigation);
---
+++ pages/Login.js
@@ -0,0 +1,5 @@
+export default function Login() {
+    return (
+        <text>Olá 2</text>
+    )
+}
